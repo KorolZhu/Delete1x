f63356fdbef5b2620a1224154ce524e6d010dd1f
Zz <zhuzhi2013@gmail.com>
2019-04-11 11:28:28 +0800

    Initial Commit

---
+++ Delete1x/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delete1x
+//
+//  Created by Zz on 2019/4/11.
+//  Copyright © 2019 Zz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
